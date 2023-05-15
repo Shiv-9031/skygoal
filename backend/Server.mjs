b7e49b6cf6a96d07a9d72b67d02f246818c071a1
unknown <kumarshiv9031127789@gmail.com>
2023-05-15 09:48:27 +0530

    remove import file

--- backend/Server.mjs
+++ backend/Server.mjs
@@ -5,7 +5,8 @@ import {database} from "./config/database.mjs"
 import cors from "cors";
 import authRoutes from "./Routes/AuthRoutes.mjs"
 import blogRoutes from "./Routes/blogRoutes.mjs"
-import path from "path"
+import path from "path";
+import { fileURLToPath} from "url"
 
 
 const app =express();
@@ -25,11 +26,18 @@ app.use(cors())
 app.use("/api/v1/auth",authRoutes)
 app.use("/api/v1/blogging",blogRoutes)
 
+//for director path
+
+//const __filename = fileURLToPath(import.meta.url);
+
+//const __dirname = path.dirname(__filename);
+//console.log(path.join())
+
 //static files
-app.use(express.static(path.join(__dirname,"./frontend/build")));
-app.get("*",function(req,res){
-    res.sendFile(path.join(__dirname,"./frontend/build/index.html"));
-})
+// app.use(express.static(path.join(__dirname,"./frontend/build")));
+// app.get("*",function(req,res){
+//     res.sendFile(path.join(__dirname,"./frontend/build/index.html"));
+// })
 
 
 
